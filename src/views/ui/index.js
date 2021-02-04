0bc502a88fbdb4d54f5291cbc2df928b4d781560
monotraine <syamsudin@eklankuteams.com>
2021-02-04 21:56:45 +0700

    remove unnecessary component

--- src/views/ui/index.js
+++ src/views/ui/index.js
@@ -1,13 +1,7 @@
 import Button from './Button';
-import Card from './Card';
-import Header from './Header';
 import Input from './Input';
-import Switch from './Switch';
 
 export {
   Button,
-  Card,
   Input,
-  Header,
-  Switch, 
 };
